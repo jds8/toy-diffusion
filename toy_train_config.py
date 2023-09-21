9192db00760fa0d8aa72906ddae35bc3e4ed437f
Justice Sefas <jsefas@cs.ubc.ca>
2023-09-21 15:37:46 -0700

    add classifier_lr

--- toy_train_config.py
+++ toy_train_config.py
@@ -24,6 +24,7 @@ class BaseConfig:
 class TrainConfig(BaseConfig):
     batch_size: int = 1024
     lr: float = 0.0001
+    classifier_lr: float = 0.0001
     no_wandb: bool = 1
     delete_local_model: bool = False
     max_gradient: float = 1.
