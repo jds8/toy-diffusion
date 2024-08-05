cf3be5da3feb5058b377d66aaee97dc2a30ad19b
Justice Sefas <jsefas@cs.ubc.ca>
2024-08-04 19:49:03 -0700

    include importance_sampling.py

--- importance_sampling.py
+++ importance_sampling.py
@@ -3,11 +3,9 @@
 from typing import Callable
 import torch
 
-from toy_sample import ToyEvaluator
-
 
 class Proposal:
-    def __init__(self, std: ToyEvaluator):
+    def __init__(self, std):
         self.std = std
         self.trajs = None
         self.ode_llk = None
@@ -26,6 +24,76 @@ class GaussianProposal(Proposal):
         scale_factor = torch.tensor(self.std.cfg.example.sigma).log()
         self.ode_llk = raw_ode_llk - scale_factor
         return self.ode_llk
+
+
+class BrownianMotionDiffProposal(Proposal):
+    def __init__(self, std):
+        super().__init__(std)
+        self.dt = torch.tensor(1/(self.std.example.sde_steps-1))
+
+    def sample(self):
+        sample_traj_out = self.std.sample_trajectories()
+        sample_trajs = sample_traj_out.samples
+        self.trajs = sample_trajs[-1] * self.dt.sqrt()
+        return self.trajs
+
+    def log_prob(self, samples):
+        sample_trajs = samples / self.dt.sqrt()
+        raw_ode_llk = self.std.ode_log_likelihood(sample_trajs)[0]
+        scale_factor = self.dt.sqrt().log() * self.std.cfg.example.sde_steps
+        self.ode_llk = raw_ode_llk - scale_factor
+        return self.ode_llk
+
+
+class Target:
+    def __init__(self, cfg):
+        self.cfg = cfg
+    def log_prob(self, saps):
+        raise NotImplementedError
+    def analytical_prob(self):
+        return -1.
+
+
+class GaussianTarget(Target):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self.dist = torch.distributions.Normal(
+            cfg.example.mu,
+            cfg.example.sigma
+        )
+    def log_prob(self, saps):
+        return self.dist.log_prob(saps)
+    def analytical_prob(self, alpha):
+        # 0.0027 for self.cfg.cond=3
+        return 2*self.dist.cdf(
+            self.cfg.example.mu - alpha * self.cfg.example.sigma
+        )
+
+class BrownianMotionDiffTarget(Target):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        self.dt = torch.tensor(1. / (cfg.example.sde_steps-1))
+        self.dist = torch.distributions.Normal(
+            0.,
+            self.dt.sqrt(),
+        )
+    def log_prob(self, saps):
+        # the elements of saps are dependent, but those of
+        # saps.diff(dim=1) are independent increments
+        return self.dist.log_prob(saps.diff(dim=1)).sum(dim=1)
+    def analytical_prob(self, alpha):
+        # 0.3336 for alpha=3
+        dist = torch.distributions.Normal(
+            0.,
+            (
+                self.dt * torch.linspace(
+                    1,
+                    self.cfg.example.sde_steps-1,
+                    self.cfg.example.sde_steps-1
+                )
+            ).sqrt(),
+        )
+        return 1 - (1 - 2 * dist.cdf(-alpha)).prod()
 
 
 class ImportanceSampler:
