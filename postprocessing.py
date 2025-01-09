8efebb8b0ba11b69b50131d015ec6a02e5859c42
Justice Sefas <jsefas@cs.ubc.ca>
2025-01-09 14:55:54 -0800

    don't reassign sample_data but an index of it!

--- postprocessing.py
+++ postprocessing.py
@@ -528,7 +528,7 @@ def make_performance_v_samples(cfg):
                 data = torch.load(file_path, map_location=device, weights_only=True)
                 rnd = get_round(file_path)
                 if sample_data[rnd] is not None:
-                    sample_data = torch.cat([
+                    sample_data[rnd] = torch.cat([
                         sample_data[rnd],
                         data.reshape(-1, 1, 1)
                     ]).reshape(-1, 1, 1)
