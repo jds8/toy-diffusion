add normalizing constant and fix naming

histogram_error_utils.py
@@ -6,8 +6,38 @@ import einops
 from compute_quadratures import get_2d_pdf
 from hydra.core.hydra_config import HydraConfig
 import scipy.integrate as integrate
-
+import matplotlib.pyplot as plt
+import einops
 from typing import Optional
+
+import os
+
+import re
+import imageio
+
+
+# provenance: https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
+def _create_gif(file_dir, gif_name=None):
+    filenames = os.listdir(file_dir)
+    filenames.sort(key=natural_keys)
+    gif_name = gif_name if gif_name is not None else filenames[0]
+    images = []
+    for filename in filenames:
+        jpg = os.path.join(file_dir, filename)
+        if jpg.endswith('.jpg'):
+            images.append(imageio.imread(jpg))
+    imageio.mimsave('{}/{}.gif'.format(file_dir, gif_name), images)
 
 
 #########################
@@ -20,9 +50,9 @@ class AnalyticalCalculator:
         raise NotImplementedError
 
 class DensityCalculator(AnalyticalCalculator):
-    def __init__(self, dist):
+    def __init__(self, dist, alpha):
         """ dist should be a scipy distribution """
-        self.dist = dist
+        self.dist = lambda x: dist.pdf(x) / (1 - dist.cdf(alpha))
     def compute(self, bins: np.ndarray, alpha: np.ndarray):
         analytical_props = self.dist.cdf(bins[1:]) - \
             self.dist.cdf(bins[:-1])
@@ -1319,6 +1349,8 @@ class ErrorMeasure:
         return self.error(analytical_props, empirical_props, bins)
     def error(self, analytical_props, empirical_props, bins):
         raise NotImplementedError
+    def clean_up(self):
+        return
 
 # class MaxError(ErrorMeasure):
 #     def error(self, analytical_props, empirical_props):
@@ -1364,6 +1396,9 @@ class SimpsonsMISE(ErrorMeasure):
         return 'SimpsonsMISE'
 
 class MISE(ErrorMeasure):
+    def __init__(self):
+        self.hist_approx_dir = f'{HydraConfig.get().run.dir}/histogram_approximations'
+        os.makedirs(self.hist_approx_dir, exist_ok=True)
     def error(
         self,
         analytical_calculator: AnalyticalCalculator,
@@ -1371,10 +1406,18 @@ class MISE(ErrorMeasure):
         bins: np.ndarray
     ):
         x_grid = np.linspace(bins[:-1], bins[1:], 20)
-        mse = ((analytical_calculator.dist.pdf(x_grid) - empirical_props) ** 2).sum()
+        mse = ((analytical_calculator.dist(x_grid) - empirical_props) ** 2).sum()
+        x_unique, idx = np.unique(einops.rearrange(x_grid, 'b w -> (w b)'), return_index=True)
+        props = einops.repeat(empirical_props, 'c -> (c w)', w=x_grid.shape[0])
+        plt.plot(x_unique, props[idx])
+        plt.plot(x_unique, analytical_calculator.dist(x_unique))
+        plt.savefig(f'{self.hist_approx_dir}/histogram_approximation_{len(bins)}.jpg')
+        plt.clf()
         return mse
     def label(self):
         return 'Integrated MSE'
+    def clean_up(self):
+        _create_gif(self.hist_approx_dir)
 
 #########################
 #### /Error Measures ####

toy_sample.py
@@ -664,6 +664,7 @@ def plot_histogram_errors(
         analytical_calculator=analytical_calculator,
         error_measure=error_measure,
     )
+    error_measure.clean_up()
     plt.clf()
     fig, (ax1, ax2) = plt.subplots(2, 1)
     ax1.plot(subsample_sizes, errors, label=title_prefix)
@@ -839,6 +840,7 @@ def plot_theta_from_sample_trajs(end_time, cfg, sample_trajs, std):
     plt.clf()
 
 def plot_chi_from_sample_trajs(
+        title_prefix,
         cfg,
         sample_trajs,
         std,
@@ -1014,7 +1016,7 @@ def test_multivariate_gaussian(
 
     title_prefix = f'{cfg.example.d}D_MVN_diffusion'
     dd = stats.chi(cfg.example.d)
-    calculator = DensityCalculator(dd)
+    calculator = DensityCalculator(dd, alpha)
     error = MISE()
     hist_errors_output = plot_histogram_errors(
         title_prefix,
@@ -1579,17 +1581,20 @@ def sample(cfg):
                     self.samples = samples
             if type(std.example) == MultivariateGaussianExampleConfig:
                 dim = cfg.example.d
+                dist_type = 'MVN'
             elif type(std.example) == BrownianMotionDiffExampleConfig:
                 dim = cfg.example.sde_steps - 1
+                dist_type = 'BM'
             sample_traj_out = A(torch.randn(10*cfg.num_samples, dim, 1))
             if std.cond == 1:
                 cond_idx = (sample_traj_out.samples.norm(dim=[1, 2]) > std.likelihood.alpha)
                 cond_samples = sample_traj_out.samples[cond_idx][:cfg.num_samples]
                 sample_traj_out.samples = cond_samples
             sample_traj_out.samples = sample_traj_out.samples.unsqueeze(0)
-            title_prefix = 'analytical'
+            title_prefix = f'analytical_{dim}D_{dist_type}'
             dd = stats.chi(dim)
-            calculator = DensityCalculator(dd)
+            alpha = torch.tensor([std.likelihood.alpha]) if std.cond == 1. else torch.tensor([0.])
+            calculator = DensityCalculator(dd, alpha)
             error = MISE()
             errors = plot_histogram_errors(
                 title_prefix,
@@ -1600,7 +1605,7 @@ def sample(cfg):
                 error_measure=error,
                 other_histogram_data=other_histogram_data,
             )
-            other_histogram_data = HistogramData(errors, 'analytical')
+            other_histogram_data = HistogramData(errors, title_prefix)
             print('DEBUG is on!')
 
         sample_traj_out = std.sample_trajectories(