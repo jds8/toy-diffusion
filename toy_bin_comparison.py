da590fae6592bfc52532c3dfa8408f0f7fbee102
Justice Sefas <jsefas@cs.ubc.ca>
2025-05-05 20:36:58 -0700

    add bin_comparison

---
+++ toy_bin_comparison.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import warnings
+import os
+import logging
+from typing import Callable, List, Tuple
+import time
+import re
+from collections import namedtuple
+import einops
+import math
+
+import pandas as pd
+import hydra
+from hydra.core.config_store import ConfigStore
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import OmegaConf
+import torch
+import scipy
+import matplotlib.pyplot as plt
+
+from toy_configs import register_configs
+from toy_sample import ContinuousEvaluator
+from toy_train_config import SampleConfig, get_run_type, MultivariateGaussianExampleConfig, \
+    BrownianMotionDiffExampleConfig
+from models.toy_diffusion_models_config import ContinuousSamplerConfig
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+ErrorData = namedtuple('ErrorData', 'x median error_bars label color')
+HistOutput = namedtuple('HistOutput', 'hist bins')
+
+#########################
+#########################
+def suppresswarning():
+    warnings.warn("user", UserWarning)
+
+def save_pfode_errors(all_num_bins, rel_errors_tensor):
+    headers = [
+        'NumBins',
+        'Errors',
+    ]
+    data = [[
+        all_num_bins,
+        rel_errors_tensor
+    ]]
+    df = pd.DataFrame(data, columns=headers)
+    filename = 'pfode_bin_comparison_errors.csv'
+    df.to_csv(
+        f'{HydraConfig.get().run.dir}/{filename}',
+        index=False
+    )
+
+def save_pfode_samples(abscissa, chi_ode_llk):
+    headers = [
+        'Abscissa',
+        'ChiOdeLLk',
+    ]
+    data = [[
+        abscissa,
+        chi_ode_llk
+    ]]
+    df = pd.DataFrame(data, columns=headers)
+    num_bins = abscissa.shape[0]
+    rel_directory = 'pfode_bin_comparison_data'
+    abs_directory = f'{HydraConfig.get().run.dir}/{rel_directory}'
+    os.makedirs(abs_directory, exist_ok=True)
+    filename = f'{abs_directory}/pfode_bin_comparison_data_{num_bins}_bins.csv'
+    df.to_csv(
+        f'{HydraConfig.get().run.dir}/{filename}',
+        index=False
+    )
+
+@hydra.main(version_base=None, config_path="conf", config_name="continuous_is_config")
+def sample(cfg):
+    logger = logging.getLogger("main")
+    logger.info('run type: importance sampling')
+    cfg_str = OmegaConf.to_yaml(cfg)
+    logger.info(f"CONFIG\n{cfg_str}")
+
+    os.system('echo git commit: $(git rev-parse HEAD)')
+
+    omega_sampler = OmegaConf.to_object(cfg.sampler)
+    if isinstance(omega_sampler, ContinuousSamplerConfig):
+        std = ContinuousEvaluator(cfg=cfg)
+    else:
+        raise NotImplementedError
+
+    with torch.no_grad():
+        alpha = std.likelihood.alpha.reshape(-1, 1)
+
+        if type(std.example) == MultivariateGaussianExampleConfig:
+            dim = cfg.example.d
+        elif type(std.example) == BrownianMotionDiffExampleConfig:
+            dim = cfg.example.sde_steps
+        else:
+            raise NotImplementedError
+
+        dd = scipy.stats.chi(dim)
+        analytical_tail = 1 - dd.cdf(alpha)
+        max_sample = dd.ppf(0.99999)
+        all_num_bins = [1000, 10000, 100000]
+        rel_errors = []
+        for num_bins in all_num_bins:
+            abscissa = torch.linspace(alpha.item(), max_sample, num_bins+1)
+            fake_traj = torch.cat([
+                torch.zeros(num_bins+1, dim-1),
+                abscissa.reshape(-1, 1)
+            ], 1).unsqueeze(-1)
+            ode_llk = std.ode_log_likelihood(
+                fake_traj,
+                cond=torch.tensor([-1.]),
+                alpha=alpha,
+                exact=cfg.compute_exact_trace,
+            )
+            chi_ode_llk = ode_llk[0][-1] + (dim / 2) * torch.tensor(2 * torch.pi).log() + \
+                (dim - 1) * abscissa.log() - (dim / 2 - 1) * \
+                torch.tensor(2.).log() - scipy.special.loggamma(dim / 2)
+            tail_estimate = scipy.integrate.simpson(chi_ode_llk.exp(), x=abscissa)
+            rel_error = (tail_estimate - analytical_tail).abs() / analytical_tail
+            rel_errors.append(rel_error)
+            save_pfode_samples(abscissa, chi_ode_llk)
+        rel_errors_tensor = torch.tensor(rel_errors)
+        save_pfode_errors(all_num_bins, rel_errors_tensor)
+        plt.plot(all_num_bins, rel_errors_tensor)
+
+
+if __name__ == "__main__":
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        suppresswarning()
+
+    cs = ConfigStore.instance()
+    cs.store(name="vpsde_sample_config", node=SampleConfig)
+    register_configs()
+
+    with torch.no_grad():
+        sample()
